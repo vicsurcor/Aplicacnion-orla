9c129d614ec138790a5323b295fe946f6855dd23
Victor <vdsurcor@gmail.com>
2023-11-12 23:31:23 +0100

    Fin

--- AplicacionOrla/MainWindow.xaml.cs
+++ AplicacionOrla/MainWindow.xaml.cs
@@ -24,31 +24,6 @@ namespace AplicacionOrla
         {
             InitializeComponent();
         }
-
-        private void MouseEnter41(object sender, MouseEventArgs e)
-        {
-            LabelPuesto41.Text = "Aritz Perez";
-        }
-
-        private void MouseLeave41(object sender, MouseEventArgs e)
-        {
-            LabelPuesto41.Text = "Aritz P";
-        }
-
-        private void MouseClick41(object sender, RoutedEventArgs e)
-        {
-            LabelPuesto41.Text = "aperezdmorr@educacion.navarra.es";
-        }
-
-        private void DoubleClick41(object sender, MouseButtonEventArgs e)
-        {
-            {
-                
-                Perfil win2 = new Perfil("Aritz", "Perez",
-                "email_alumno@educacion.navarra.es", "Imagenes/Usuario.jpg");
-                win2.Show();
-            }
-        }
     }
 
 }
--- AplicacionOrla/Puesto.xaml.cs
+++ AplicacionOrla/Puesto.xaml.cs
@@ -15,14 +15,68 @@ using System.Windows.Shapes;
 
 namespace AplicacionOrla
 {
+
     /// <summary>
     /// Lógica de interacción para UserControl1.xaml
     /// </summary>
     public partial class Puesto : UserControl
     {
+        public static DependencyProperty NombreProperty =
+                      DependencyProperty.Register("Nombre", typeof(string), typeof(Puesto),
+                      new PropertyMetadata(string.Empty));
+        public static DependencyProperty ApellidosProperty =
+                      DependencyProperty.Register("Apellidos", typeof(string), typeof(Puesto),
+                      new PropertyMetadata(string.Empty));
+        public static DependencyProperty EmailProperty =
+                      DependencyProperty.Register("Email", typeof(string), typeof(Puesto),
+                      new PropertyMetadata(string.Empty));
+        public static DependencyProperty FotoProperty =
+                      DependencyProperty.Register("Foto", typeof(string), typeof(Puesto),
+                      new PropertyMetadata(string.Empty));
+
         public Puesto()
         {
+            
             InitializeComponent();
+            LabelPuesto.Text = Nombre;
+
+        }
+        public string Nombre
+        {
+            get { return (string)GetValue(NombreProperty); }
+            set { SetValue(NombreProperty, value); }
+        }
+        public string Apellidos
+        {
+            get { return (string)GetValue(ApellidosProperty); }
+            set { SetValue(ApellidosProperty, value); }
+        }
+        public string Email
+        {
+            get { return (string)GetValue(EmailProperty); }
+            set { SetValue(EmailProperty, value); }
+        }
+        public string Foto
+        {
+            get { return (string)GetValue(FotoProperty); }
+            set { SetValue(FotoProperty, value); }
+        }
+        private void MouseEnterP(object sender, MouseEventArgs e)
+        {
+            LabelPuesto.Text = Nombre + " " + Apellidos;
+        }
+        private void MouseLeaveP(object sender, MouseEventArgs e)
+        {
+            LabelPuesto.Text = "";
+        }
+        private void ClickP(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto.Text = Email;
+        }
+        private void DoubleClickP(object sender, MouseButtonEventArgs e)
+        {
+            Perfil win32 = new Perfil(Nombre, Apellidos, Email, Foto);
+            win32.Show();
         }
     }
 }
